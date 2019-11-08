bcd2155c3d43cb68d151a7da5abb81ff9b4e1dbc
Yingqing Chen <yingqing.chen@autodesk.com>
2019-11-08 18:31:45 +0800

    modify script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,12 +4,11 @@ pytest --cov=src -s --cov-config=.coveragerc > result.txt
 cat result.txt 
 if cat result.txt | grep "failed"
 then
-  
+  echo "Test failed"
   exit 2
 fi
 echo ----Check coverage rate, target: 100%----
 coverage report -m  > cov.txt
-cat cov.txt
 coverage_rate=$(cat cov.txt | tail -n 1 | grep -Eo '[^ ]+$' | tr -d %)
 if [ $coverage_rate -gt 90 ]
 then
