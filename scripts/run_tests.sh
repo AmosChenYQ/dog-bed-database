0e91028d9832c8ed1c09af443d1897d9a7b1f926
Yingqing Chen <yingqing.chen@autodesk.com>
2019-11-08 18:27:42 +0800

    add shell script to travis

---
+++ scripts/run_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo ----Run unit tests and generate coverage data----
+pytest --cov=src -s --cov-config=.coveragerc > result.txt
+cat result.txt 
+if cat result.txt | grep "failed"
+then
+  
+  exit 2
+fi
+echo ----Check coverage rate, target: 100%----
+coverage report -m  > cov.txt
+cat cov.txt
+coverage_rate=$(cat cov.txt | tail -n 1 | grep -Eo '[^ ]+$' | tr -d %)
+if [ $coverage_rate -gt 90 ]
+then
+  echo "Python code coverage checking failed!"
+  coverage html
+  exit 2
+fi
