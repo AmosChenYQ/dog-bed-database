7934b7e7bfad4f5a07c018b347943a0cd56f1bd9
Yingqing Chen <yingqing.chen@autodesk.com>
2019-11-18 09:21:35 +0800

    Add some unittest

--- tests/test_interface.py
+++ tests/test_interface.py
@@ -17,5 +17,34 @@ class TestDBDB(object):
         self.dbdb.close()
         with pytest.raises(ValueError, match='Database closed.'):
             self.dbdb._assert_not_closed()
+
+    def test_can_set_and_get(self):
+        self.dbdb["a"] = "1"
+        self.dbdb.commit()
+        eq_(self.dbdb["a"], "1")
     
-    
+    def test_can_delete(self):
+        self.dbdb["a"] = "1"
+        self.dbdb.commit()
+        eq_(self.dbdb["a"], "1")
+        del self.dbdb["a"]
+        with pytest.raises(KeyError):
+            val = self.dbdb["a"]
+
+    def test_can_set_and_contain_returns_true(self):
+        self.dbdb["a"] = "1"
+        self.dbdb.commit()
+        eq_("a" in self.dbdb, True)
+        eq_(self.dbdb["a"], "1")
+        eq_("b" in self.dbdb, False)
+
+    def test_can_get_correct_length(self):
+        self.dbdb["a"] = "1"
+        self.dbdb.commit()
+        self.dbdb["b"] = "2"
+        self.dbdb.commit()
+        self.dbdb["c"] = "3"
+        self.dbdb.commit()
+        eq_(len(self.dbdb), 3)
+        
+        
